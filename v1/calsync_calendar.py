3bf6aba7c36e2d52cb88733061b957739ffb3e05
joaquim <joaquim.perez@he-arc.ch>
2016-12-08 20:28:01 +0100

    icsCalendar.read_events : gestion des deux types date et datetime + date de fin. GoogleCalendar.write_event ok

--- v1/calsync_calendar.py
+++ v1/calsync_calendar.py
@@ -1,70 +1,78 @@
-import datetime
+from datetime import date, datetime, timezone
 
 import icalendar
+from icalendar import vDatetime, vDate, vDDDTypes
 
 from google_api_tools import get_service
 
 
 class Calendar:
-	"""Classe représeantant un calendrier abstrait"""
-	def __init__(self):
-		self.events=[]
+    """Classe représeantant un calendrier abstrait"""
 
-	def print_events(self):
-		if not self.events:
-			print('No upcoming events found.')
-		for event in self.events:
-			# récupère la date et l'heure si dispo, sinon seulement la date
-			start = event['start'].get('dateTime',event['start'].get('date'))
-			print(start, event['summary'])
+    def __init__(self):
+        self.events = []
+
+    def print_events(self):
+        if not self.events:
+            print('No upcoming events found.')
+        for event in self.events:
+            # récupère la date et l'heure si dispo, sinon seulement la date
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
 
 
 class IcsCalendar(Calendar):
+    def __init__(self, ics_path):
+        Calendar.__init__(self)
+        with open(ics_path, encoding="utf-8") as ics_text:
+            self.ical = icalendar.Calendar.from_ical(ics_text.read())
 
-	def __init__(self, ics_path):
-		Calendar.__init__(self)
-		with open(ics_path, encoding="utf-8") as ics_text:
-			self.ical = icalendar.Calendar.from_ical(ics_text.read())
+    def read_events(self):
+        for component in self.ical.walk():
+            if component.name == "VEVENT":
+                event = {}
+                event["summary"] = component.get("summary")+"" # avoid vText()
 
+                for param in ["start","end"]:
+                    event[param] = {}
+                    d = component.get('dt'+param)
+                    ddd = vDDDTypes.from_ical(d)
+                    if type(ddd) is datetime:
+                        event[param]["dateTime"] = str(ddd.isoformat())
+                    elif type(ddd) is date:
+                        event[param]["date"] = str(ddd)
 
-	def read_events(self):
-		for component in self.ical.walk():
-			if component.name == "VEVENT":
-				event={}
-				event["summary"] = component.get("summary")
-				event["start"]={}
-				event["start"]["dateTime"] = str(component.decoded("dtstart"))
-				self.events.append(event)
+                self.events.append(event)
 
-# pas une si bonne idée car readonly				
-# class OnlineIcsCalendar(IcsCalendar):
+    def write_event(self, event):
+        pass
 
-	# def __init__(self, url):
-		# Calendar.__init__(self)
-		# import urllib.request
-		# self.ical = icalendar.Calendar.from_ical(urllib.request.urlopen(url).read())
-		
 
 class GoogleCalendar(Calendar):
-	def __init__(self, id="primary"):
-		Calendar.__init__(self)
-		self.service = get_service()
-		self.id=id
-		self.calendar = self.service.calendars().get(calendarId=id).execute()
+    def __init__(self, id="primary"):
+        Calendar.__init__(self)
+        self.service = get_service()
+        self.id = id
+        self.calendar = self.service.calendars().get(calendarId=id).execute()
 
-	def read_events(self):
-		now = datetime.datetime.utcnow().isoformat() + 'Z'	# 'Z' indicates UTC time
-		eventsResult = self.service.events().list(
-			calendarId=self.id, singleEvents=True,
-			orderBy='startTime').execute()
-		self.events = eventsResult.get('items', [])
-		
-	def list_calendars(self):
-		page_token = None
-		while True:
-			calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
-			for calendar_list_entry in calendar_list['items']:
-				print(calendar_list_entry['summary'])
-			page_token = calendar_list.get('nextPageToken')
-			if not page_token:
-				break
+    def read_events(self):
+        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        eventsResult = self.service.events().list(
+            calendarId=self.id, singleEvents=True,
+            orderBy='startTime').execute()
+        self.events = eventsResult.get('items', [])
+
+    def list_calendars(self):
+        page_token = None
+        while True:
+            calendar_list = self.service.calendarList().list(pageToken=page_token).execute()
+            for calendar_list_entry in calendar_list['items']:
+                print(calendar_list_entry['summary'])
+            page_token = calendar_list.get('nextPageToken')
+            if not page_token:
+                break
+
+    def write_event(self, event):
+        event['summary'] += " (Calsync)"
+        e = self.service.events().insert(calendarId=self.id, body=event).execute()
+        print('Event created: %s' % (e.get('htmlLink')))
--- v1/use_calendar.py
+++ v1/use_calendar.py
@@ -1,26 +1,47 @@
 from calsync_calendar import IcsCalendar, GoogleCalendar
 import json
 
-if __name__ =="__main__":
+if __name__ == "__main__":
 
-	with open("calsync.conf.json", encoding="utf-8") as f:
-		calendars={}
-		config = json.loads(f.read())
-		for key,params in config["definitions"].items():
-			if params["type"] == "ics":
-				calendars[key]=(IcsCalendar(params["path"]))
-			elif params["type"] == "google":
-				calendars[key]=(GoogleCalendar(params["id"]))
-		for k,cal in calendars.items():
-			print(k)
-			cal.read_events()
-			cal.print_events()
-		for i,rule in enumerate(config["rules"]):
-			print("rule {} : {} = {} {} {}".format(
-				i,
-				rule["destination"],
-				rule["operands"][0],
-				rule["operation"],
-				rule["operands"][1]
-				)
-			)
+    with open("./calsync.conf.json", encoding="utf-8") as f:
+        calendars = {}
+        config = json.loads(f.read())
+        print("Definitions :")
+        for key, params in config["definitions"].items():
+            if params["type"] == "ics":
+                calendars[key] = (IcsCalendar(params["path"]))
+            elif params["type"] == "google":
+                calendars[key] = (GoogleCalendar(params["id"]))
+        for k, cal in calendars.items():
+            print("{} ({})".format(k, cal.__class__.__name__))
+            cal.read_events()
+            cal.print_events()
+
+        print("Rules :")
+        for i, rule in enumerate(config["rules"]):
+            print("rule {} : {} = {} {} {}".format(
+                i,
+                rule["destination"],
+                rule["operands"][0],
+                rule["operation"],
+                rule["operands"][1]
+            ))
+
+        #e1 = calendars["B"].events[0]
+        #e2 = calendars["A"].events[0]
+        #calendars["C"].write_event(e1)
+        #calendars["C"].write_event(e2)
+
+        e = {
+            'start': {
+                'date': '2016-12-20'
+            },
+            'end': {
+                'date': '2016-12-21'
+            },
+            'summary': 'Yolo'
+        }
+        #calendars['B'].write_event(e)
+
+        for e in calendars['A'].events:
+            calendars['B'].write_event(e)
