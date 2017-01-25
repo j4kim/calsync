264f65c2649537b9fc20f28496f07a053fa3b00d
joaquim <joaquim.perez@he-arc.ch>
2017-01-26 00:43:11 +0100

    cryptage du mot de passe exchange par une clé en dure dans le code. Du coup non c'est pas sécurisé mais au moins ça évite d'avoir le mot de passe écrit en clair dans un fichier

--- v1/exchange_calendar.py
+++ v1/exchange_calendar.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 from exchangelib import DELEGATE, Account, Credentials, \
@@ -5,30 +6,31 @@ from exchangelib import DELEGATE, Account, Credentials, \
 
 from calsync_calendar import CalsyncCalendar
 from exchange_event import ExchangeEvent, to_exchange
+from simplecrypt import encrypt, decrypt
+from getpass import getpass
 
 class ExchangeCalendar(CalsyncCalendar):
-    def __init__(self, name, server, username, address, pwd=None):
+    def __init__(self, name, server, username, address):
         CalsyncCalendar.__init__(self, name)
 
-        if not pwd:
-            try:
-                with open(".exchange_pwd", encoding="utf-8") as pwd_file:
-                    pwd = pwd_file.read()
-            except:
-                # from getpass import getpass
-                # pwd = getpass("Entrez votre mot de passe Exchange : ")
-                pwd = input("Entrez votre mot de passe Exchange (attention, il va être affiché) : ")
+        try:
+            with open(".exchange_pwd", encoding="utf-8") as pwd_file:
+                pwd = decrypt('calsync', pwd_file.read())
+        except:
+            pwd = getpass("Entrez votre mot de passe Exchange : ")
+            with open(".exchange_pwd", 'wb') as pwd_file:
+                pwd_file.write(encrypt("calsync", pwd))
 
         credentials = Credentials(username=username, password=pwd, is_service_account=False)
         try:
-            self.account = Account(primary_smtp_address=address,
-                                   config=Configuration(
-                                       server=server,credentials=credentials,auth_type=NTLM
-                                   ),
-                                   access_type=DELEGATE,
-                                   locale="ch_fr")
+            self.account = Account(
+                primary_smtp_address=address,
+                config=Configuration(server=server,credentials=credentials,auth_type=NTLM),
+                access_type=DELEGATE,
+                locale="ch_fr")
         except:
             print("connexion au compte {} impossible".format(username), file=sys.stderr)
+            os.remove(".exchange_pwd")
             sys.exit()
         self.read_events()
 
